181fb38c89b25d06b4d7e6e6a3217192a3d07193
Yoshida Takuto <ej17a095@oecu.jp>
2022-07-05 15:47:21 +0900

    論文 直前

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# KHR3_001_description
+# choreonoid_ros_khr3
 
 - Based on https://qiita.com/ackermanrf128/items/cab9d1983435ece9e000
 
 
 - Terminal 
   ```
-  roslaunch KHR3_001_description bringup_group.launch
+  roslaunch choreonoid_ros_khr3 bringup_group.launch
 
   ```
 - Termina2 
   ```
-  rosrun KHR3_001_description sinewave_command_group
+  rosrun choreonoid_ros_khr3 sinewave_command_group
 
   ```
--- src/sinewave_command_group.cpp
+++ src/sinewave_command_group.cpp
@@ -4,10 +4,11 @@
 std_msgs::Float64MultiArray cmd;
 ros::Publisher command_pub;
 
-const double l_shoulder_pitch_amplitude = 1.0;
-const double l_shoulder_roll_amplitude = 1.5;
-const double l_shoulder_pitch_freq = 0.5;
-const double l_shoulder_roll_freq = 1.0;
+const double c_chest_yaw_amplitude = 1.0;
+const double c_head_yaw_amplitude = 1.5;
+
+const double c_chest_yaw_freq = 0.5;
+const double c_head_yaw_freq = 1.0;
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "choreonoid_ros_khr3_control_group");
@@ -21,9 +22,28 @@ int main(int argc, char** argv){
     while(ros::ok()){
         double current_time = (ros::Time::now() - t_start).toSec();
 
-        cmd.data.resize(2);
-        cmd.data[0] = l_shoulder_pitch_amplitude * sin(2 * M_PI * l_shoulder_pitch_freq * current_time);
-        cmd.data[1] = l_shoulder_roll_amplitude * sin(2 * M_PI * l_shoulder_roll_freq * current_time);
+        cmd.data.resize(22);
+        cmd.data[0] = c_chest_yaw_amplitude * sin(2 * M_PI * c_chest_yaw_freq * current_time);
+        cmd.data[1] = 0;
+        cmd.data[2] = 0;
+        cmd.data[3] = 0;
+        cmd.data[4] = 0;
+        cmd.data[5] = 0;
+        cmd.data[6] = 0;
+        cmd.data[7] = 0;
+        cmd.data[8] = 0;
+        cmd.data[10] = 0;
+        cmd.data[11] = c_head_yaw_amplitude * sin(2 * M_PI * c_head_yaw_freq * current_time);
+        cmd.data[12] = 0;
+        cmd.data[13] = 0;
+        cmd.data[14] = 0;
+        cmd.data[15] = 0;
+        cmd.data[16] = 0;
+        cmd.data[17] = 0;
+        cmd.data[18] = 0;
+        cmd.data[19] = 0;
+        cmd.data[20] = 0;
+        cmd.data[21] = 0;
 
         command_pub.publish(cmd);
 
